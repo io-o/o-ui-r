89a9694a4eac466c5fb157b9f4842a21b221893a
kevinl7 <mygao666@gmail.com>
2020-04-28 22:26:46 +0800

    :tada: add test

---
+++ src/components/o-button/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import OButton from './index'
+
+test('O-button', () => {
+  const wrapper = render(<OButton>OButton</OButton>)
+  const element = wrapper.queryByText('OButton')
+  expect(element).toBeTruthy()
+})
