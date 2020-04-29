0dde790e73caedc7de77a0c00f15302b1f8968ee
kevinl7 <mygao666@gmail.com>
2020-04-29 23:04:22 +0800

    :tada: add menu

---
+++ src/components/o-menu/menu.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import classNames from 'classnames'
+
+type MenuMode = 'horizonatal' | 'vertical'
+
+export interface MenuProps {
+  defaultIndex?: number;
+  className?: string;
+  mode?: MenuMode;
+  style?: React.CSSProperties;
+  onSelect?: (selectIndex: number) => void;
+}
+
+const OMenu: React.FC<MenuProps> = (props) => {
+  const {
+    className,
+    mode,
+    style,
+    children,
+    defaultIndex
+  } = props
+
+  const classes = classNames('o-menu', className, {
+    'menu-vertical': mode === 'vertical'
+  })
+
+  return (
+    <ul className={classes} style={style}>
+      {children}
+    </ul>
+  )
+
+}
+
+OMenu.defaultProps = {
+  defaultIndex: 0,
+  mode: 'horizonatal'
+}
+
+
+export default OMenu
---
+++ src/components/o-menu/menuItem.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export interface MenuItemProps {
+  index?: number;
+  disabled?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const OMenuItem: React.FC<MenuItemProps> = (props) => {
+  const {
+    className,
+    index,
+    style,
+    children,
+    disabled
+  } = props
+
+  const classes = classNames('o-menu-item', className, {
+    'is-disabled': disabled
+  })
+
+  return (
+    <li className={classes} style={style}>
+      {children}
+    </li>
+  )
+
+}
+
+OMenuItem.defaultProps = {
+}
+
+
+export default OMenuItem
